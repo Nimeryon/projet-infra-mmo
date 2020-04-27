cb670f709756bd2360122a3b4ba47b12362e7662
Thomas AURIOL <thomas.auriol@ynov.com>
2020-04-27 21:37:53 +0200

    push

--- test/mapCheatSheet.js
+++ test/mapCheatSheet.js
@@ -3,12 +3,15 @@
 MOVE TO
 
 */
+var timeGlobA = Date.now();
+var scale = 2;
+
 PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
 
 const app = new PIXI.Application(
     {
-        width: 32 * 8 * 3 * 3,
-        height: 32 * (6 * 23) * 3,
+        width: 32 * 8 * 3 * scale,
+        height: 32 * (6 * 23) * scale,
         backgroundColor: 0xAAAAAA,
         // resizeTo: window
     }
@@ -31,6 +34,7 @@ loader.add('tileset_wall', "sprites/tilesets/wall.png");
 loader.add('tileset_grass', "sprites/tilesets/grass.png");
 // Execution
 loader.load((loader, resources) => {
+    let timeA = Date.now();
     function generateTextures(texture, nbr_tile_x, nbr_tile_y, tile_size_x, tile_size_y, notATile = []) {
         let textures = [];
         let frame_count = 0;
@@ -143,20 +147,23 @@ loader.load((loader, resources) => {
 
     function generateAnimatedTileset(textures) {
         let tileset = [];
-        let texture_index = 0;
-        let count = 0;
+        let tileset_i = 0;
+        let notAFrame = [47, 95, 143, 191, 239, 287, 335, 383, 431, 479, 527];
+        let frameCount = 0;
         for (let texture_i = 0; texture_i < textures.length; texture_i++) {
-            for (let nbr_frame = 0; nbr_frame < textures[texture_i.nbr_frame]; i++) {
-                for (let y = 0; y < textures[texture_i].height; y++) {
-                    for (let x = 0; x < textures[texture_i].width; x++) {
-                        if (notATile.indexOf(count) == -1) {
-                            tileset[texture_index] = new PIXI.Texture(textures[texture_i].texture,
-                                new PIXI.Rectangle(x * 32, y * 32, 32, 32)
+            for (let y = 0; y < textures[texture_i].height; y++) {
+                for (let x = 0; x < textures[texture_i].width; x++) {
+                    if (notAFrame.indexOf(frameCount) == -1) {
+                        let frames = [];
+                        for (let nbr_frame = 0; nbr_frame < textures[texture_i].nbr_frame; nbr_frame++) {
+                            frames[nbr_frame] = new PIXI.Texture(textures[texture_i].texture,
+                                new PIXI.Rectangle((x + (nbr_frame * 8)) * 32, y * 32, 32, 32)
                             );
-                            texture_index++;
                         }
-                        count++;
-                    }
+                        tileset[tileset_i] = frames;
+                        tileset_i++;
+                    }   
+                    frameCount++;
                 }
             }
         }
@@ -198,21 +205,23 @@ loader.load((loader, resources) => {
 
     let animated_tileset_textures = [
         {
-            texture: 'tileset_water',
+            texture: resources['tileset_water'].texture,
             nbr_frame: 8,
             width: 8,
-            height: 6 * 3
+            height: 6 * 8
         },
         {
-            texture: 'tileset_waterfall',
+            texture: resources['tileset_waterfall'].texture,
             nbr_frame: 4,
             width: 8,
-            height: 6 * 8
+            height: 6 * 3
         }
     ]
 
-    let animated_tileset = generateAnimatedTileset(animated_tileset_textures);
-    let tileset = generateTileset(tileset_textures);
+    let tileset = {
+        animated: generateAnimatedTileset(animated_tileset_textures),
+        normal: generateTileset(tileset_textures)
+    }
 
     let notATile = [1111, 1159, 1207, 1255, 1303, 1351, 1399, 1447, 1495, 1543, 1591, 1639, 1687, 1735, 1783, 1831, 1879, 1927, 1975, 2023, 2071, 2119];
     let text_count = 0;
@@ -221,19 +230,19 @@ loader.load((loader, resources) => {
         for (let x = 0; x < 8; x++) {
             if (notATile.indexOf(count) == -1) {
                 if (text_count <= 1063) {
-                    let sprite = createSprite(tileset[text_count], x * 32 * 3, y * 32 * 3, 1, 0, {x: 3, y: 3});
+                    let sprite = createSprite(tileset.normal[text_count], x * 32 * scale, y * 32 * scale, 1, 0, {x: scale, y: scale});
                     app.stage.addChild(sprite);
-                    let text = new PIXI.Text(String(text_count));
-                    text.x = 32 * 3 * x + (32 * 3 / 2);
-                    text.y = 32 * 3 * y + (32 * 3 / 2);
+                    let text = new PIXI.Text(String(text_count), {fontSize: 10 * scale});
+                    text.x = 32 * scale * x + (32 * scale / 2);
+                    text.y = 32 * scale * y + (32 * scale / 2);
                     text.anchor.set(0.5);
                     app.stage.addChild(text);
                 } else {
-                    let sprite = createSprite(tileset[text_count], (x + 8) * 32 * 3, (y - (6 * 22 + 1)) * 32 * 3, 1, 0, {x: 3, y: 3});
+                    let sprite = createSprite(tileset.normal[text_count], (x + 8) * 32 * scale, (y - (6 * 22 + 1)) * 32 * scale, 1, 0, {x: scale, y: scale});
                     app.stage.addChild(sprite);
-                    let text = new PIXI.Text(String(text_count));
-                    text.x = 32 * 3 * (x + 8) + (32 * 3 / 2);
-                    text.y = 32 * 3 * (y - (6 * 22 + 1)) + (32 * 3 / 2);
+                    let text = new PIXI.Text(String(text_count), {fontSize: 10 * scale});
+                    text.x = 32 * scale * (x + 8) + (32 * scale / 2);
+                    text.y = 32 * scale * (y - (6 * 22 + 1)) + (32 * scale / 2);
                     text.anchor.set(0.5);
                     app.stage.addChild(text);
                 }
@@ -242,6 +251,27 @@ loader.load((loader, resources) => {
             count++;
         }
     }
+
+    count = 0;
+    text_count = 0;
+    let notAFrame = [47, 95, 143, 191, 239, 287, 335, 383, 431, 479, 527];
+    for (let y = 0; y < 6 * 11; y++) {
+        for (let x = 0; x < 8; x++) {
+            if (notAFrame.indexOf(count) == -1) {
+                let sprite = createAnimatedSprite(tileset.animated[text_count], (x + 16) * 32 * scale, y * 32 * scale, 1, 0, {x: scale, y: scale}, 0.1, true);
+                app.stage.addChild(sprite);
+                let text = new PIXI.Text(String(text_count), {fontSize: 10 * scale});
+                text.x = 32 * scale * (x + 16) + (32 * scale / 2);
+                text.y = 32 * scale * y + (32 * scale / 2);
+                text.anchor.set(0.5);
+                app.stage.addChild(text);
+                text_count++;
+            }
+            count++;
+        }
+    }
+    console.log(`loading time = ${Date.now() - timeA} ms`);
+    console.log(`global loading time = ${Date.now() - timeGlobA} ms`);
 });
 
 loader.onProgress.add(e => {
