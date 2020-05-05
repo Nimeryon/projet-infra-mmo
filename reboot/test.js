56458ea9a0b572ac48f98f1288004a228dd99e0a
Thomas AURIOL <thomas.auriol@ynov.com>
2020-05-05 22:53:59 +0200

    suppréssion reboot

--- reboot/test.js
+++
@@ -1,7 +0,0 @@
-// Connection base de donnée
-const mongojs = require('mongojs');
-const db = mongojs('192.168.43.87:27017/mmo-project', ['account', 'progress']);
-
-db.account.find({ username: "Nimerya" }, function (err, res) {
-    console.log(err, res);
-});
